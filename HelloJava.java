ebcd4c78ee90405917fcde1ee9f23b657a4b3c83
heenu <heenupathak06@gmail.com>
2019-07-16 21:47:11 +0100

    added print ln statement for testing jenkins build

--- HelloJava.java
+++ HelloJava.java
@@ -3,10 +3,10 @@ public class HelloJava{
  	public static void main(String[] args) {
  		for (int i=1; i<=10; i++)
 		{
-			System.out.println("Hello , This is small java program");
 
 			System.out.println("Hello Java  " + i);
 		}
+		System.out.println(" java program for testing Jenkins build");
  	}
 		
 	
