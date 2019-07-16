3e2a4738783aaa5702fbf75ab90440a629e0ce33
heenu <heenupathak06@gmail.com>
2019-07-16 21:41:43 +0100

    Print statement  added to program

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,8 @@ public class HelloJava{
  	public static void main(String[] args) {
  		for (int i=1; i<=10; i++)
 		{
+			System.out.println("Hello , This is small java program");
+
 			System.out.println("Hello Java  " + i);
 		}
  	}
